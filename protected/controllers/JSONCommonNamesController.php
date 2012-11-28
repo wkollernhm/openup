7f47333fd8cbad4f0af5d71ef6877df653ec85b5
wkollernhm <wolfgang.koller@nhm-wien.ac.at>
2012-11-28 13:20:14 +0100

    handling of multi-queries added

--- protected/controllers/JSONCommonNamesController.php
+++ protected/controllers/JSONCommonNamesController.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * JSON Controller for handling requests to the common names webservice
+ * For details on the actual service see http://open-up.eu/content/common-names-service
  */
 class JSONCommonNamesController extends Controller {
     public function japiGetCommonNames($query = null, $queries = null) {
@@ -14,11 +15,18 @@ class JSONCommonNamesController extends Controller {
         }
         // check for single query mode
         else if( $query != null ) {
-            // decode json query
-            $query = json_decode($query, true);
-            
-            // check for valid query
-            if( $query == null ) {
+            $return = $this->handleQuery($query);
+        }
+        // multi-query mode?
+        else if( $queries != null ) {
+            // check if we have a valid queries array
+            if( is_array($queries) ) {
+                // handle each sub-query separately
+                foreach( $queries as $query ) {
+                    $return[] = $this->handleQuery($query);
+                }
+            }
+            else {
                 header('HTTP/1.0 400 Bad Request', true, 400);
                 exit();
             }
@@ -27,6 +35,29 @@ class JSONCommonNamesController extends Controller {
         return $return;
     }
     
+    /**
+     * handle a single query and return the result
+     * @param string $query Query as JSON-String
+     * @return array response according to webservice specification
+     */
+    private function handleQuery($query) {
+        $response = array();
+        
+        // decode json query
+        $query = json_decode($query, true);
+
+        // check for valid query
+        if( $query == null ) {
+            header('HTTP/1.0 400 Bad Request', true, 400);
+            exit();
+        }
+        
+        return $response;
+    }
+    
+    /**
+     * Define this controller as JSON controller
+     */
     public function actions() {
         return array(
             'japi'=>'JApi',
