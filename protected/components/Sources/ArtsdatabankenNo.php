f4df92681da1d6048b6add955f4d7a9bb72d2db7
wkollernhm <wolfgang.koller@nhm-wien.ac.at>
2013-02-25 11:58:33 +0100

    code cleanup

--- protected/components/Sources/ArtsdatabankenNo.php
+++ protected/components/Sources/ArtsdatabankenNo.php
@@ -51,54 +51,9 @@ class ArtsdatabankenNo extends CachedSoapClient {
                         "references" => array('artsdatabanken.no'),
                         "taxon" => $LatinskNavn->VitenskapligNavn,
                     );
-                    //var_dump($Navn);
                 }
             }
         }
-        
-//        if( isset($records->ArtssokResult->LatinskNavn) ) {
-//            foreach($records->ArtssokResult->LatinskNavn as $latinskNavn) {
-//                var_export($latinskNavn);
-//                foreach( $latinskNavn->Popularnavn as $popularnavn) {
-//                    foreach($popularnavn->Navn as $navn) {
-//                        $response[] = array(
-//                            "name" => $navn->_,
-//                            "language" => $popularnavn->Spraak,
-//                            'geography' => NULL,
-//                            'period' => NULL,
-//                            "score" => 100,
-//                            "match" => true,
-//                            "references" => array('artsdatabanken.no'),
-//                            "taxon" => $latinskNavn->VitenskapligNavn,
-//                        );
-//                    }
-//                }
-//            }
-//        }
-        
-//        if( is_array($records) ) {
-//            // Check all records and analyze the data
-//            foreach( $records as $record ) {
-//                $guid = $record->GUID;
-//
-//                $vernaculars = $this->getPESIVernacularsByGUID( $guid );
-//
-//                foreach( $vernaculars as $vernacular ) {
-//                    if( !isset( $vernacular->vernacular ) ) continue;
-//
-//                    $response[] = array(
-//                        "name" => $vernacular->vernacular,
-//                        "language" => $vernacular->language_code,
-//                        'geography' => NULL,
-//                        'period' => NULL,
-//                        "score" => 100,
-//                        "match" => true,
-//                        "references" => array('pesi'),
-//                        "taxon" => $record->scientificname,
-//                    );
-//                }
-//            }
-//        }
         
         return $response;
     }
