325651c044a84d57efb0d4001bc4d5d68a322661
wkollernhm <wolfgang.koller@nhm-wien.ac.at>
2013-02-25 11:47:41 +0100

    using artsdatabanken as source is now possible

--- protected/components/Sources/ArtsdatabankenNo.php
+++ protected/components/Sources/ArtsdatabankenNo.php
@@ -21,8 +21,61 @@ class ArtsdatabankenNo extends CachedSoapClient {
         $response = array();
         
         // Fetch records matching our query
-        $records = $this->Artssok( $term );
-        var_export($records);
+        $records = $this->Artssok( array('Search' => $term) );
+        $ArtssokResult = $records->ArtssokResult;
+        
+        // check for found response
+        if( isset($ArtssokResult->LatinskNavn) ) {
+            $LatinskNavn = $ArtssokResult->LatinskNavn;
+            $Takson = $LatinskNavn->Takson;
+            
+            // create fake array for single result entries
+            $Popularnavns = $Takson->Popularnavn;
+            if( !is_array($Popularnavns) ) $Popularnavns = array( $Popularnavns );
+
+            // add all popularnavn
+            foreach( $Popularnavns as $Popularnavn ) {
+                // create fake array for single result entries
+                $Navns = $Popularnavn->Navn;
+                if( !is_array($Navns) ) $Navns = array($Navns);
+                
+                // cycle through actual names and add them
+                foreach( $Navns as $Navn ) {
+                    $response[] = array(
+                        "name" => $Navn->_,
+                        "language" => $Popularnavn->Spraak,
+                        'geography' => NULL,
+                        'period' => NULL,
+                        "score" => 100,
+                        "match" => true,
+                        "references" => array('artsdatabanken.no'),
+                        "taxon" => $LatinskNavn->VitenskapligNavn,
+                    );
+                    //var_dump($Navn);
+                }
+            }
+        }
+        
+//        if( isset($records->ArtssokResult->LatinskNavn) ) {
+//            foreach($records->ArtssokResult->LatinskNavn as $latinskNavn) {
+//                var_export($latinskNavn);
+//                foreach( $latinskNavn->Popularnavn as $popularnavn) {
+//                    foreach($popularnavn->Navn as $navn) {
+//                        $response[] = array(
+//                            "name" => $navn->_,
+//                            "language" => $popularnavn->Spraak,
+//                            'geography' => NULL,
+//                            'period' => NULL,
+//                            "score" => 100,
+//                            "match" => true,
+//                            "references" => array('artsdatabanken.no'),
+//                            "taxon" => $latinskNavn->VitenskapligNavn,
+//                        );
+//                    }
+//                }
+//            }
+//        }
+        
 //        if( is_array($records) ) {
 //            // Check all records and analyze the data
 //            foreach( $records as $record ) {
