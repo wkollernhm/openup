0288fe8b788fe0130848f75b130fc4afe9d3444a
wkollernhm <wolfgang.koller@nhm-wien.ac.at>
2013-02-26 14:43:32 +0100

    cleaning code

--- protected/components/Sources/NewZealandLandcare.php
+++ protected/components/Sources/NewZealandLandcare.php
@@ -8,32 +8,33 @@ class NewZealandLandcare extends SourceComponent {
     public function query($term) {
         $response = array();
         
+        // filter list using a like search
         $dbCriteria = new CDbCriteria();
         $dbCriteria->addSearchCondition('NameFull', $term);
         
+        // find all matching entries
         $models_newZealandLandcare = SourceNewZealandLandcare::model()->findAll($dbCriteria);
-        if( $models_newZealandLandcare != NULL ) {
-            foreach($models_newZealandLandcare as $model_newZealandLandcare) {
-                $references = array('New Zealand Landcare');
-                if( $model_newZealandLandcare->ReferenceGenCitation != NULL ) $references[] = $model_newZealandLandcare->ReferenceGenCitation;
+        // cycle through models and add them to result
+        foreach($models_newZealandLandcare as $model_newZealandLandcare) {
+            // we add the new zealand landcare as a reference, since sometimes the field is empty
+            $references = array('New Zealand Landcare');
+            if( $model_newZealandLandcare->ReferenceGenCitation != NULL ) $references[] = $model_newZealandLandcare->ReferenceGenCitation;
 
-                // parse the returned name and compare it to the actual query term, in order to only return 100% matches
-                $nameFull = Yii::app()->NameParser->parse($model_newZealandLandcare->NameFull);
-                if( $nameFull != $term ) continue;
-                
-                // construct response data
-                $response[] = array(
-                    "name" => $model_newZealandLandcare->VernacularName,
-                    "language" => $model_newZealandLandcare->LanguageISOCode,
-                    "geography" => $model_newZealandLandcare->GeoRegionName,
-                    "period" => NULL,
-                    "taxon" => $nameFull,
-                    "references" => $references,
-                    "score" => 100,
-                    "match" => true,
-                );
-            }
-            
+            // parse the returned name and compare it to the actual query term, in order to only return 100% matches
+            $nameFull = Yii::app()->NameParser->parse($model_newZealandLandcare->NameFull);
+            if( $nameFull != $term ) continue;
+
+            // construct response data
+            $response[] = array(
+                "name" => $model_newZealandLandcare->VernacularName,
+                "language" => $model_newZealandLandcare->LanguageISOCode,
+                "geography" => $model_newZealandLandcare->GeoRegionName,
+                "period" => NULL,
+                "taxon" => $nameFull,
+                "references" => $references,
+                "score" => 100,
+                "match" => true,
+            );
         }
         
         return $response;
