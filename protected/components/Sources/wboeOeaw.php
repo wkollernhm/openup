b949c6e47afd3aea2a747b39e4af59c4577bf10a
wkollernhm <wolfgang.koller@nhm-wien.ac.at>
2013-02-27 15:56:04 +0100

    added wbö data as source for common names

--- protected/components/Sources/wboeOeaw.php
+++ protected/components/Sources/wboeOeaw.php
@@ -8,16 +8,47 @@ class wboeOeaw extends CachedRESTClient {
     public function init() {
         parent::init();
         
-        $this->m_url = 'http://wboe.oeaw.ac.at/api/taxonid/';
+        $this->url = 'http://wboe.oeaw.ac.at/api/taxonid/';
         $this->m_timeout = 2592000;     // 30 days caching time
     }
     
+    /**
+     * Query the WBÖ service based on taxonIDs provided by the NHMW system
+     * @param string $term Term to search for
+     * @return boolean
+     */
     public function query($term) {
         $response = array();
         
         // use the generic NHMW service for querying
         $matches = Yii::app()->NHMWService->query($term);
-        var_dump($matches);
+        // go through all matches and add them as result
+        foreach( $matches['result'] as $result ) {
+            foreach( $result['searchresult'] as $searchresult ) {
+                if( isset($searchresult['species']) && is_array($searchresult['species']) ) {
+                    foreach( $searchresult['species'] as $species ) {
+                        // ask wboe for each taxonID we've found
+                        $results = json_decode($this->getResponse($species['taxonID']), true);
+                        // check for a avalid response
+                        if( $results == NULL ) continue;
+                        
+                        // check all results and add them to response
+                        foreach($results as $result) {
+                            $response[] = array(
+                                "name" => $result['name'],
+                                "language" => $result['language'],
+                                'geography' => NULL,
+                                'period' => NULL,
+                                "taxon" => $species['taxon'],
+                                "references" => $result['reference'],
+                                "score" => $species['ratio'] * 100.0,
+                                "match" => ($species['ratio'] == 1) ? true : false,
+                            );
+                        }
+                    }
+                }
+            }
+        }
         
         return $response;
     }
