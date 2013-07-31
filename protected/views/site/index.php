7ef243c12a53170ff5d4fa86a5e83e6b9c40f0aa
wkollernhm <wolfgang.koller@nhm-wien.ac.at>
2013-07-31 16:26:57 +0200

    note for configuration error

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -5,3 +5,5 @@ $this->pageTitle=Yii::app()->name;
 ?>
 
 <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+
+If you see this page, you have to configure your server to support Rewriting. Make sure mod_rewrite is enabled and .htaccess overwriting is allowed.
