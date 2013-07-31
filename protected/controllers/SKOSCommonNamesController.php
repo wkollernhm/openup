c5b0a8175900f6fcccf2fa833b8963a1c002e339
wkollernhm <wolfgang.koller@nhm-wien.ac.at>
2013-07-31 17:29:37 +0200

    action for common names query in SKOS added

--- protected/controllers/SKOSCommonNamesController.php
+++ protected/controllers/SKOSCommonNamesController.php
@@ -3,5 +3,7 @@
  * Controller class for common names output in SKOS Format
  */
 class SKOSCommonNamesController extends Controller {
-    
+    public function actionGetCommonNames($query = NULL, $queries = NULL) {
+        
+    }
 }
