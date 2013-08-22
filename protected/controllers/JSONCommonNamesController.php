ff3e102493445d64071370e3afe9adb00a7ee965
wkollernhm <wolfgang.koller@nhm-wien.ac.at>
2013-08-22 13:09:50 +0200

    merged source querying into the SourceComponent class for common handling

--- protected/controllers/JSONCommonNamesController.php
+++ protected/controllers/JSONCommonNamesController.php
@@ -4,6 +4,12 @@
  * For details on the actual service see http://open-up.eu/content/common-names-service
  */
 class JSONCommonNamesController extends Controller {
+    /**
+     * Main entry function for querying for common names using the Reconciliation API
+     * @param array $query
+     * @param array $queries
+     * @return array
+     */
     public function japiGetCommonNames($query = NULL, $queries = NULL) {
         $return = array( 'result' => array() );
 
@@ -52,108 +58,8 @@ class JSONCommonNamesController extends Controller {
             exit();
         }
         
-        // parse the name before sending it to the services
-        $query['query'] = Yii::app()->NameParser->parse($query['query']);
-
-        // ask all sources
-        $sources = SourceComponent::getSources();
-        foreach ($sources as $source) {
-            $sourceResponse = $source->query($query['query']);
-
-            // check for valid response
-            if( is_array($sourceResponse) ) {
-                $response = array_merge($response, $sourceResponse);
-            }
-        }
-
-        // deduplicate response before returning it
-        return $this->deduplicateResponse($response);
-    }
-    
-    /**
-     * deduplicates a set of results returned from the individual sources
-     * @param type $response
-     * @return type
-     */
-    private function deduplicateResponse($response) {
-        $dedupResponse = array();
-        
-        // handle each result and deduplicate it
-        foreach($response as $result) {
-            // try to find cached version of common name
-            $model_commonNamesCache = CommonNamesCache::model()->findByAttributes(array(
-                'name' => $result['name'],
-                'language' => $result['language'],
-                'geography' => $result['geography'],
-                'period' => $result['period'],
-            ));
-            // if this name wasn't cached yet, create it
-            if( $model_commonNamesCache == NULL ) {
-                $model_commonNamesCache = new CommonNamesCache();
-                $model_commonNamesCache->name = $result['name'];
-                $model_commonNamesCache->language = $result['language'];
-                $model_commonNamesCache->geography = $result['geography'];
-                $model_commonNamesCache->period = $result['period'];
-                // check if saving the cached entry worked
-                if( !$model_commonNamesCache->save() ) {
-                    continue;
-                }
-            }
-            
-            // clean the scientific name
-            $result['taxon'] = Yii::app()->NameParser->parse($result['taxon']);
-            
-            // find the scientific name
-            $model_scientificName = ScientificNameCache::model()->findByAttributes(array(
-                'name' => $result['taxon']
-            ));
-            // create cached scientific name if it doesn't exist yet
-            if( $model_scientificName == NULL ) {
-                $model_scientificName = new ScientificNameCache();
-                $model_scientificName->name = $result['taxon'];
-                // check if saving the cached entry worked
-                if( !$model_scientificName->save() ) {
-                    continue;
-                }
-            }
-            
-            // check if this common name already exists in results
-            if( isset($dedupResponse[$model_commonNamesCache->id])
-                && $dedupResponse[$model_commonNamesCache->id]['taxon_id'] == $model_scientificName->id ) {
-                // just update references
-                if( is_array($result['references']) ) {
-                    $dedupResponse[$model_commonNamesCache->id]['references'] = array_merge(
-                        $dedupResponse[$model_commonNamesCache->id]['references'],
-                        $result['references']
-                    );
-                }
-            }
-            // if not add a new entry
-            else {
-                $dedupResponse[$model_commonNamesCache->id] = array(
-                    // common name info
-                    'id' => $model_commonNamesCache->id,
-                    'name' => $model_commonNamesCache->name,
-                    'type' => array('/name/common'),
-                    'language' => $model_commonNamesCache->language,
-                    'geography' => $model_commonNamesCache->geography,
-                    'period' => $model_commonNamesCache->period,
-                    // scientific name info
-                    'taxon' => $model_scientificName->name,
-                    'score' => $result['score'],
-                    'match' => $result['match'],
-                    'taxon_id' => $model_scientificName->id,
-                    'references' => $result['references']
-                );
-            }
-        }
-        
-        // for compatibility reasons, add reference as concatenated string
-        foreach($dedupResponse as &$dedupResult) {
-            $dedupResult['reference'] = join(';', $dedupResult['references']);
-        }
-        
-        return array_values($dedupResponse);
+        // query sources and return them
+        return SourceComponent::querySources($query['query']);
     }
 
     /**
