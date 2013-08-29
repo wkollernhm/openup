e5a2ec71f96a5e21eec7ecf476a02dfc43d2b1d7
wkollernhm <wolfgang.koller@nhm-wien.ac.at>
2013-08-29 14:16:24 +0200

    fixing additional whitespace

--- docs/sources/tbl_source_slovak_bratislava_import.sql
+++ docs/sources/tbl_source_slovak_bratislava_import.sql
@@ -3,4 +3,4 @@ use openup;
 
 -- change path to csv file downloaded from source, make sure separator is the semicolon (';')
 LOAD DATA INFILE '/var/www/html/common_names_fungi_20120823.xls' INTO TABLE tbl_source_slovak_bratislava CHARACTER SET 'utf8' COLUMNS TERMINATED BY ';' ENCLOSED BY '"' IGNORE 1 LINES;
- LOAD DATA INFILE '/var/www/html/common_names_plants_slovak_20120823.xls' INTO TABLE tbl_source_slovak_bratislava CHARACTER SET 'utf8' COLUMNS TERMINATED BY ';' ENCLOSED BY '"' IGNORE 1 LINES;
+LOAD DATA INFILE '/var/www/html/common_names_plants_slovak_20120823.xls' INTO TABLE tbl_source_slovak_bratislava CHARACTER SET 'utf8' COLUMNS TERMINATED BY ';' ENCLOSED BY '"' IGNORE 1 LINES;
