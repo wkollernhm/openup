78e1d360f6b5cb9d30e8190628cdae8e30ccb5be
wkollernhm <wolfgang.koller@nhm-wien.ac.at>
2013-08-30 09:12:56 +0200

    documentation for class

--- protected/widgets/CHttpCache.php
+++ protected/widgets/CHttpCache.php
@@ -1,29 +1,38 @@
 <?php
 
+/**
+ * Caching class based on COutputCache but also includes the HTTP headers
+ */
 class CHttpCache extends COutputCache {
 
     protected $_headers = array();
 
+    /**
+     * Marks the end of content to be cached.
+     * Content displayed before this method call and after {@link init()}
+     * will be captured and saved in cache.
+     * This method does nothing if valid content is already found in cache.
+     */
     public function run() {
         $contentCached = $this->getIsContentCached();
-        
+
         // check for header displaying
-        if( $contentCached ) {
+        if ($contentCached) {
             // re-display all headers
             foreach ($this->_headers as $header) {
                 header($header);
             }
         }
-        
+
         // now run parent code
         parent::run();
-        
+
         // finally also add the header to the cached entry (if necessary)
-        if( !$contentCached ) {
+        if (!$contentCached) {
             $this->_headers = headers_list();
             $data = $this->getCache()->get($this->getCacheKey());
             $data = array($data[0], $data[1], $this->_headers);
-            $this->getCache()->set($this->getCacheKey(),$data,$this->duration,$this->dependency);
+            $this->getCache()->set($this->getCacheKey(), $data, $this->duration, $this->dependency);
         }
     }
 
@@ -32,13 +41,14 @@ class CHttpCache extends COutputCache {
      * @return boolean whether the content is found in cache.
      */
     protected function checkContentCache() {
-        if( parent::checkContentCache() ) {
+        if (parent::checkContentCache()) {
             $data = $this->getCache()->get($this->getCacheKey());
             $this->_headers = $data[2];
-            
+
             return true;
         }
-        
+
         return false;
     }
+
 }
