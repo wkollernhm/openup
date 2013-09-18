5038d2c573a433dd550ae917c4c9deb1222d4683
wkollernhm <wolfgang.koller@nhm-wien.ac.at>
2013-09-18 08:41:31 +0200

    model & component file for Linnaeus Projects source

---
+++ protected/components/Sources/LinnaeusProjects.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Common names from ETI Databases export
+ *
+ * @author wkoller
+ */
+class LinnaeusProjects extends SourceComponent {
+    public function query($term) {
+        $response = array();
+        
+        $dbCriteria = new CDbCriteria();
+        $dbCriteria->compare('Taxon', $term, true);
+        $models_sourceEtiDatabases = SourceEtiDatabases::model()->findAll($dbCriteria);
+        
+        foreach( $models_sourceEtiDatabases as $model_sourceEtiDatabases ) {
+            $response[] = array(
+                "name" => $model_sourceEtiDatabases->Name,
+                "language" => $model_sourceEtiDatabases->iso_639_6,
+                "geography" => NULL,
+                'period' => NULL,
+                "taxon" => Yii::app()->NameParser->parse($model_sourceEtiDatabases->Taxon),    // return a clean taxon name
+                "references" => array($model_sourceEtiDatabases->Source),
+                "score" => 100.0,
+                "match" => true,
+            );
+        }
+        
+        return $response;
+    }
+}
