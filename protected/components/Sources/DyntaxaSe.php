0bca868b262958f3f64784f0b466e17effce1248
wkollernhm <wolfgang.koller@nhm-wien.ac.at>
2013-09-25 12:00:04 +0200

    basic service workflow (login -> call -> logout) for dyntaxa

--- protected/components/Sources/DyntaxaSe.php
+++ protected/components/Sources/DyntaxaSe.php
@@ -44,6 +44,31 @@ class DyntaxaSe extends CachedSoapClient {
     public function query($term) {
         $response = array();
         
+        // login to the service
+        $WebLoginResponse = $this->Login(
+                array(
+                    'userName' => $this->userName, 
+                    'password' => $this->password, 
+                    'applicationIdentifier' => $this->applicationIdentifier,
+                    'isActivationRequired' => $this->isActivationRequired
+                )
+        );
+        $WebLoginResponse = $WebLoginResponse->LoginResult;
+        
+        // construct internal webclient information object
+        $WebClientInformation = array(
+            'Locale' => $WebLoginResponse->Locale,
+            'Role' => NULL,
+            'Token' => $WebLoginResponse->Token
+        );
+        
+        // Logout after calling the service
+        $this->Logout(
+                array(
+                    'clientInformation' => $WebClientInformation
+                )
+        );
+        
         return $response;
     }
 }
