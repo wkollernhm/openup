149a750f7da5274cc68aa5e2bc53b3625e0e2bd3
Wolfgang Koller <wolfgang.koller@nhm-wien.ac.at>
2013-10-08 13:12:53 +0200

    result fetching for binomials

--- protected/components/Sources/HungarianPeregovits.php
+++ protected/components/Sources/HungarianPeregovits.php
@@ -7,6 +7,33 @@
 class HungarianPeregovits extends SourceComponent {
     public function query($term) {
         $response = array();
+
+        // get components of parsed name
+        $termParsed = Yii::app()->NameParser->parse($term);
+        if( $termParsed != NULL ) {
+            $termDetails = $termParsed['details'][0];
+            
+            // find all fitting scientific name entries
+            $models_sourceHungarianPeregovits = SourceHungarianPeregovits::model()->findAllByAttributes(array(
+                'Genus' => $termDetails['genus'],
+                'species' => $termDetails['species'],
+            ));
+
+            // cycle through results and add them to the response
+            foreach($models_sourceHungarianPeregovits as $model_sourceHungarianPeregovits) {
+                // construct response data
+                $response[] = array(
+                    "name" => $model_sourceHungarianPeregovits->HU_Common_name,
+                    "language" => "hun",
+                    "geography" => NULL,
+                    "period" => $model_sourceHungarianPeregovits->Period,
+                    "taxon" => $termParsed['canonical'],
+                    "references" => array(),
+                    "score" => 100,
+                    "match" => true,
+                );
+            }
+        }
         
         return $response;
     }
