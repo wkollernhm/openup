7e3c0f60d9ed7713ff2dfd7311aa7c1c51d83a88
Wolfgang Koller <wkoller@virasnotebook.at.home>
2013-10-31 09:22:26 +0100

    updating sql dump of basic structure

--- docs/openup.sql
+++ docs/openup.sql
@@ -2,10 +2,15 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
+DROP SCHEMA IF EXISTS `openup` ;
+CREATE SCHEMA IF NOT EXISTS `openup` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `openup` ;
 
 -- -----------------------------------------------------
 -- Table `tbl_common_names_cache`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `tbl_common_names_cache` ;
+
 CREATE  TABLE IF NOT EXISTS `tbl_common_names_cache` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `name` VARCHAR(100) NOT NULL ,
@@ -20,6 +25,8 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `tbl_scientific_name_cache`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `tbl_scientific_name_cache` ;
+
 CREATE  TABLE IF NOT EXISTS `tbl_scientific_name_cache` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `name` VARCHAR(100) NOT NULL ,
@@ -30,6 +37,8 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `tbl_service`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `tbl_service` ;
+
 CREATE  TABLE IF NOT EXISTS `tbl_service` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `url` VARCHAR(255) NOT NULL ,
@@ -40,6 +49,8 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `tbl_webservice_cache`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `tbl_webservice_cache` ;
+
 CREATE  TABLE IF NOT EXISTS `tbl_webservice_cache` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `service_id` INT NOT NULL ,
@@ -68,11 +79,13 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 -- Data for table `tbl_service`
 -- -----------------------------------------------------
 START TRANSACTION;
+USE `openup`;
 INSERT INTO `tbl_service` (`id`, `url`) VALUES (1, 'http://webservice.catalogueoflife.org/webservice?response=full&format=php&name=');
 INSERT INTO `tbl_service` (`id`, `url`) VALUES (2, 'http://131.130.131.9/taxamatch/jsonRPC/json_rpc_taxamatchMdld.php');
 INSERT INTO `tbl_service` (`id`, `url`) VALUES (3, 'http://www.eu-nomen.eu/portal/soap.php?wsdl=1');
 INSERT INTO `tbl_service` (`id`, `url`) VALUES (4, 'http://webtjenester.artsdatabanken.no/Artsnavnebase.asmx?WSDL');
 INSERT INTO `tbl_service` (`id`, `url`) VALUES (5, 'http://wboe.oeaw.ac.at/api/taxonid/');
 INSERT INTO `tbl_service` (`id`, `url`) VALUES (6, 'http://ws.luomus.fi/triplestore/search?predicate=dwc:scientificName&object=');
+INSERT INTO `tbl_service` (`id`, `url`) VALUES (7, 'https://taxon.artdatabankensoa.se/TaxonService.svc?wsdl');
 
 COMMIT;
