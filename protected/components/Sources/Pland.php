0df7ea24355971cad2e7c6a899530b871540566f
wkollernhm <wolfgang.koller@nhm-wien.ac.at>
2015-08-22 20:23:14 +0200

    Batch processing functional - parameter for exact matches only - clean joining / output of common names - header line in output csv - unlink of csv prior writing - generic transcoding support in WS sources

--- protected/components/Sources/Pland.php
+++ protected/components/Sources/Pland.php
@@ -12,6 +12,7 @@ class Pland extends CachedRESTClient {
 
         $this->url = "http://www.meertens.knaw.nl/pland/rest/?format=php&plantnaam=";
         $this->m_timeout = 2592000; // 30 days
+        $this->sourceEncoding = "Windows-1252";
     }
 
     /**
@@ -22,13 +23,14 @@ class Pland extends CachedRESTClient {
     public function query($term) {
         $response = array();
 
-        $results = unserialize($this->getResponse($term));
+        $results = $this->transcode_results(unserialize($this->getResponse($term)));
 
         // check for valid response
         if ($results != null && is_array($results) && isset($results[$term]) && is_array($results[$term])) {
             foreach ($results[$term] as $result) {
                 // check for an exact match
-                $score = (strcasecmp($result['botanische_naam'], $term) == 0) ? 100 : 0;
+                $botanische_naam = Yii::app()->NameParser->clean($result['botanische_naam']);
+                $score = (strcasecmp($botanische_naam, $term) == 0) ? 100 : 0;
                 $match = ($score == 100) ? true : false;
 
                 // add main entry (nl_naam) first
@@ -63,25 +65,4 @@ class Pland extends CachedRESTClient {
 
         return $response;
     }
-
-    /**
-     * Get the resposne from a REST based service
-     * @param string $query Query to send to the REST service (appended to URL)
-     * @return mixed Response of service
-     */
-    protected function getResponse($query) {
-        // urlencode query by default
-        $query = urlencode($query);
-
-        // check for cached response of this query
-        $response = mb_convert_encoding($this->getCachedResponse($query), "UTF-8", "Windows-1252");
-        if ($response == null) {
-            // fetch the response from the rest service
-            $response = file_get_contents($this->url . $query);
-            $this->setCachedResponse($query, mb_convert_encoding($response, "Windows-1252", "UTF-8"));
-        }
-
-        return $response;
-    }
-
 }
--- protected/components/WSComponent.php
+++ protected/components/WSComponent.php
@@ -1,10 +1,12 @@
 <?php
+
 /**
  * Basic Webservice component class
  *
  * @author wkoller
  */
 abstract class WSComponent extends SourceComponent {
+
     /**
      * Internal reference variable for the service id, needs to be set in the sub-implementation
      * @var int ID of registered service
@@ -16,13 +18,19 @@ abstract class WSComponent extends SourceComponent {
      * @var int 
      */
     protected $m_timeout = 86400;
-    
+
     /**
      * URI to identify this webservice
      * @var string 
      */
     private $m_url = null;
-    
+
+    /**
+     * If the service uses a different encoding than UTF-8, you can specify it here to do an automatic transcoding to/from UTF-8
+     * @var String
+     */
+    protected $sourceEncoding = NULL;
+
     /**
      * Setter function for URL, automatically checks the service for validity
      * @param string $value URI to identify the webservice with
@@ -30,19 +38,19 @@ abstract class WSComponent extends SourceComponent {
      */
     protected function setUrl($value) {
         $this->m_url = $value;
-        
+
         // find the service definition & check validity
         $model_service = Service::model()->findByAttributes(array(
             'url' => $this->m_url
         ));
-        if( $model_service == null ) {
+        if ($model_service == null) {
             throw new Exception("Invalid service");
         }
-        
+
         // remember service id
         $this->m_service_id = $model_service->id;
     }
-    
+
     /**
      * Return the URL of this service
      * @return type
@@ -50,51 +58,50 @@ abstract class WSComponent extends SourceComponent {
     protected function getUrl() {
         return $this->m_url;
     }
-    
+
     /**
      * Returns the cached response for a given query
      * @param mixed $query Query to look for
      * @return mixed null if no valid response was found, else the response
      */
     protected function getCachedResponse($query) {
-        if( $this->m_service_id == null ) {
+        if ($this->m_service_id == null) {
             throw new Exception('Set service Id before using the cache!');
         }
         $query = sha1(serialize($query));   // using SHA1 to make this quicker
-        
         // find cached entry for this query
         $dbCriteria = new CDbCriteria();
         $dbCriteria->compare('service_id', $this->m_service_id);
         $dbCriteria->compare('query', $query);
         // make sure it's recent enough
         $dbCriteria->compare('timestamp', '>=' . (time() - $this->m_timeout));
-        
+
         // only the most recent entry
         $dbCriteria->order = 'timestamp DESC';
         $dbCriteria->limit = 1;
         $model_webserviceCache = WebserviceCache::model()->find($dbCriteria);
-        
+
         // check for valid cache entry
-        if( $model_webserviceCache != null ) {
+        if ($model_webserviceCache != null) {
             return unserialize($model_webserviceCache->response);
         }
 
         return null;
     }
-    
+
     /**
      * Store a response in the cache
      * @param mixed $query Query to cache this response for
      * @param mixed $response Response to cache
      * @throws Exception
      */
-    protected function setCachedResponse($query,$response) {
-        if( $this->m_service_id == null ) {
+    protected function setCachedResponse($query, $response) {
+        if ($this->m_service_id == null) {
             throw new Exception('Set service Id before using the cache!');
         }
         $query = sha1(serialize($query));   // using SHA1 to make this quicker
         $response = serialize($response);
-        
+
         // remember new values and cache them
         $model_webserviceCache = new WebserviceCache();
         $model_webserviceCache->service_id = $this->m_service_id;
@@ -103,7 +110,7 @@ abstract class WSComponent extends SourceComponent {
         $model_webserviceCache->timestamp = time();
         $model_webserviceCache->save();
     }
-    
+
     /**
      * Helper function for cleaning outdated cache entries
      */
@@ -113,8 +120,30 @@ abstract class WSComponent extends SourceComponent {
         // only delete outdated entries
         $dbCriteria->compare('timestamp', '<' . (time() - $this->m_timeout));
         $dbCriteria->compare('service_id', '=' . $this->m_service_id);
-        
+
         // finally delete all matching entries
         WebserviceCache::model()->deleteAll($dbCriteria);
     }
+
+    /**
+     * Transcode the result to UTF-8, if necessary NOTE: this needs to be explicitly called from your implementation
+     * @param array $result
+     * @return array
+     */
+    protected function transcode_results($result) {
+        if ($this->sourceEncoding == NULL) {
+            return $result;
+        }
+
+        foreach ($result as $key => $value) {
+            if (is_array($value)) {
+                $array[$key] = $this->transcode_results($value);
+            } else {
+                $array[$key] = mb_convert_encoding($value, $this->sourceEncoding, 'UTF-8');
+            }
+        }
+
+        return $result;
+    }
+
 }
